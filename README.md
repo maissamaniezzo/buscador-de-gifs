5035505337c9ab57756c381a4bd4b57b6d40e324
Maissa Maniezzo de Oliveira <maissamaniezzo@gmail.com>
2021-02-04 19:34:16 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto buscador de gifs
+
+Projeto desenvolvido para treinamento Dart/Flutter durante trainee byron.solutions.
+
+Proposta proveniente do curso: [Criação de Apps Android e iOS com Flutter - Crie 16 Apps](https://www.udemy.com/course/curso-completo-flutter-app-android-ios/)
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      // hintColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)), 
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple[400])),
+        hintStyle: TextStyle(color: Colors.amber),
+      )
+    ),
+  ));
+}
+
